43f2719b1dccf6d30ca11725113f71c80c1d06da
khr777 <kim5638yw@gmail.com>
2021-06-06 18:19:50 +0900

    spring boot, jpa 연습 프로젝트 시작

---
+++ src/main/java/site/cancod/MyCancodApplication.java
@@ -0,0 +1,13 @@
+package site.cancod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCancodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCancodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/cancod/MyCancodApplicationTests.java
@@ -0,0 +1,13 @@
+package site.cancod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCancodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
